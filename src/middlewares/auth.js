087547d4862a7467ad596bb048b933888365c7eb
farhandz <ammarjoz09@gmail.com>
2021-06-17 16:31:20 +0700

    acl

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -8,6 +8,7 @@ const verifyCallback = (req, resolve, reject, requiredRights) => async (err, use
     return reject(new ApiError(httpStatus.UNAUTHORIZED, 'Please authenticate'));
   }
   req.user = user;
+  // eslint-disable-next-line no-console
 
   if (requiredRights.length) {
     const userRights = roleRights.get(user.role);
